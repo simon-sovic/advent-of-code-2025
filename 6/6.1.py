bfce0226cd0ea2b7e8d32822e2d4f66b02f08efe
Simon Sovič <dev@strigido.com>
2025-12-08 14:30:16 +0100

    add Ashlyn's comment fixes

--- 6/6.1.py
+++ 6/6.1.py
@@ -2,10 +2,12 @@
 with open('input', 'r') as file:
     tokens = [ line.split() for line in file.read().split('\n') if line.strip() != '' ]
 
+# Separate the operators from the numbers
+operators = tokens.pop()
 
-operators = tokens.pop()    # separate the operators from the numbers
-numbers = list(map(list, zip(*tokens))) # transpose the remaining matrix of numbers so that
-                                        # each problem's numbers are in their own line
+# Transpose the remaining matrix of numbers so that
+#  each problem's numbers are in their own line
+numbers = list(map(list, zip(*tokens)))
 
 answer = sum( eval(operator.join(nums)) for nums, operator in zip(numbers, operators) )
 print(answer)
